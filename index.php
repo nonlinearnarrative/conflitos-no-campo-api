daa2f79be1421c75bc91c5d911e9a8524257b879
David van Gelder de Neufville <_@neufv.website>
2018-09-06 12:52:31 +0200

    Add case files

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
-$gf = new GoogleFeed();
 
-$app->get('/', function ($request, $response, $args) {
-  forEach($gf->getFolder('0Byo7P47EvrO9ZU1BdHpMUFVNN00') as $file) {
-    print_r($file);
-  }
+$app->get('/{folder_id}', function ($request, $response, $args) {
+  // Get feed
+  $feed = new GoogleFeed();
+
+  // Write folder with ID
+  $response
+    ->getBody()
+    ->write(
+      json_encode(
+        $feed->getFolder($args['folder_id']),
+        true
+      )
+    );
+
+  return $response;
 });
 
 $app->run();
+
+
+//root folder id:0Byo7P47EvrO9ZU1BdHpMUFVNN00
